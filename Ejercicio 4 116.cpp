99e7c8d4d3d73f65bfe5af08ca83b74c4ef6f0ac
ralmen <33528861+ralmen@users.noreply.github.com>
2022-09-14 17:11:19 +0200

    Add files via upload

---
+++ Ejercicio 4 116.cpp
@@ -0,0 +1,18 @@
+int main ()
+{
+	 int n1, n2, n3, n4, n5, media;
+	 cout << "Introducce un numero: ";
+	 cin >>  n1;
+	 cout << "Introducce un numero: ";
+	 cin >>  n2;
+	 cout << "Introducce un numero: ";
+	 cin >>  n3;
+	 cout << "Introducce un numero: ";
+	 cin >>  n4;
+	 cout << "Introducce un numero: ";
+	 cin >>  n5;
+	 media =  n1 + n2 + n3 + n4 + n5 / 5;
+	 cout << " La media de todos es " << media << endl;
+	 
+	 return: 0;
+}
---
+++ practica.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    
+    cout<<"Introduce el primer numero: ";
+    cin>>n1;
+    cout<<"Introduce el segundo numero: ";
+    cin>>n2;
+    cout<<"El producto de "<<n1 <<" y " <<n2 <<" es " <<n1*n2;
+    
+    return 0;
+}
